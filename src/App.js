a0a102f901d84065eaf6672fc3bec8935e72e577
ann_azd <ailenzepold@gmail.com>
2024-09-09 01:59:50 -0400

    fiesta de la pia

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <div className='contenedor-imag'>
         <img className='img-p' src={imagen} />
-        <p className='parra'  >Tareas de la Piña Colada Stinky</p>
+        <p className='parra'  >Fiestas de la Piña Colada Stinky</p>
       </div>
 
       <div className='lista-tareas'>
